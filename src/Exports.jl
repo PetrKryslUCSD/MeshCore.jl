2ff582d46510be4790cb543c128053836f619b06
Petr Krysl <pkrysl@ucsd.edu>
2020-07-06 11:04:15 -0700

    Minor

--- src/Exports.jl
+++ src/Exports.jl
@@ -14,7 +14,7 @@ using ..MeshCore: nfeatofdim
 using ..MeshCore: SHAPE_DESC, NoSuchShape, P1, L2, T3, T4, Q4, H8, L3, T6, Q8
 
 export AbsShapeDesc, NoSuchShapeDesc, P1ShapeDesc, L2ShapeDesc, Q4ShapeDesc, H8ShapeDesc, T3ShapeDesc, T4ShapeDesc, L3ShapeDesc, T6ShapeDesc, Q8ShapeDesc
-export manifdim, nvertices, nfacets, nridges, n1storderv, nshifts
+export manifdim, n1storderv, nfacets, nridges, nshifts, nvertices
 export nfeatofdim
 export SHAPE_DESC, NoSuchShape, P1, L2, T3, T4, Q4, H8, L3, T6, Q8
 
