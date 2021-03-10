00b30d90fa21dd7e3b14a435919e736f0713554d
Petr Krysl <pkrysl@ucsd.edu>
2021-03-10 10:08:22 -0800

    add example of function attribute

--- src/attributes.jl
+++ src/attributes.jl
@@ -80,7 +80,7 @@ vertices.attributes["label"] = a
 ```
 
 The function attribute supports the methods `size`, `getindex`.
-It does not support `setindex!`: there is no way of changing the data.
+It does not support `setindex!`: there is no way to change the data.
 """
 struct FunAttrib{T,M,F} <: AbsAttrib{T}
     z::T
@@ -91,6 +91,10 @@ end
 Base.size(a::A) where {A<:FunAttrib} = (a.m,)
 Base.getindex(a::A, i::Int) where {A<:FunAttrib} = a.f(i)
 Base.getindex(a::A, I::Vararg{Int,N}) where {A<:FunAttrib,N} = a.f.(I...)
+# The `show` method may need some more work. It doesn't seem to play nice with @show .
+Base.show(io::IOContext{Base.TTY}, unused::MIME{Symbol("text/plain")}, a::X) where {X<:FunAttrib} = let 
+    print(io, "$(a.m)-element FunAttrib")
+end
 
 """
     datavaluetype(AbsAttrib{T}) where {T}
